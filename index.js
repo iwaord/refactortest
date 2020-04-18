00ad6196e6c813e73f32d5e3be90cde6138a7bbb
ぃゎぉ <i.w.a.o.jp@gmail.com>
2020-04-18 22:51:21 +0000

    問題は起きなかったがローカル変数を宣言 インデントがずれていた箇所を修正

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 // 料金を計算する
 function amountFor(play, perf) {
-      switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
-    return thisAmount;
+　let thisAmount =0;
+
+  switch (play.type) {
+    case "tragedy":
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy":
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+  }
+  return thisAmount;
 }
 
 
