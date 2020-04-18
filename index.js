54b6faa636283f2032e17cacb10083818b0f805a
ぃゎぉ <i.w.a.o.jp@gmail.com>
2020-04-18 22:42:54 +0000

    コード理解のための改行コードを追加

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 function statement (invoice, plays) {
   console.log(invoice)
+
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   const format = new Intl.NumberFormat("en-US",  {style: "currency", currency: "USD",  minimumFractionDigits: 2 }).format;
+
   for (let perf of invoice.performances) { 
     const play = plays[perf.playID];
     let thisAmount = 0;
@@ -25,16 +27,21 @@ function statement (invoice, plays) {
       default:
         throw new Error(`unknown type: ${play.type}`);
     }
+
     // ボリューム特典のポイントを加算
     volumeCredits += Math.max(perf.audience - 30, 0);
     // 喜劇のときは 10人につき、 さらにポイントを加算
     if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience/ 5);
+
     // 注文の内訳を出力
     result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+
     totalAmount += thisAmount;
   }
+
   result += `Amount owed is ${format(totalAmount/100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
+
   return result;
 } 
 
